b13932d2b5595ba97ee07d5e45caf90e5b22325e
Danesh Khodadad <dani.kh2652@gmail.com>
2024-08-19 21:25:03 +0330

    add js project

---
+++ js project/test js project/js test .js
@@ -0,0 +1,15 @@
+function change_background_color() {
+  document.getElementById("test-1").style.backgroundColor = "pink";
+}
+
+function change_color() {
+  document.getElementById("test-1").style.color = "white";
+}
+
+function zoom_out() {
+  document.getElementById("test-1").style.fontSize = "1rem";
+}
+
+function zoom_in() {
+  document.getElementById("test-1").style.fontSize = "4rem";
+}
