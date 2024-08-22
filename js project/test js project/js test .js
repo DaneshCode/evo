5959f59b970c0c86da144053e936c8972d336432
Danesh Khodadad <dani.kh2652@gmail.com>
2024-08-22 07:14:37 +0330

    update

--- js project/test js project/js test .js
+++ js project/test js project/js test .js
@@ -13,3 +13,11 @@ function zoom_out() {
 function zoom_in() {
   document.getElementById("test-1").style.fontSize = "4rem";
 }
+
+function turn_on() {
+  document.getElementById("turn_off").src = "./images/pic_bulbon.gif";
+}
+
+function turn_off() {
+  document.getElementById("turn_off").src = "./images/pic_bulboff.gif";
+}
